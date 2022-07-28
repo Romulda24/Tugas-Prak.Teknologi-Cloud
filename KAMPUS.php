ba9a0efc19edc5ae981746a4302cd24ea7314104
Romulda24 <westiromulda@gmail.com>
2022-07-28 13:53:57 +0700

    first commit

---
+++ KAMPUS.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.98.0">
+    <title>Kampus</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
+    
+
+<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+      .container {
+        margin-top: 15px;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<header>
+  <nav class="navbar navbar-expand-md fixed-top bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">PROFIL</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="kampus.php">KAMPUS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="moto_hidup.php">MOTO HIDUP</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+
+<main>
+
+<div class="container">
+  <div class="container marketing">
+
+<br>
+ <hr class="featurette-divider">
+     <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Sejarah Kampus</h2>
+        <p class="lead" style="font-size: 15px;">Pada tanggal 30 Juni 1979 didirikan Yayasan Pendidikan Widya Bakti Yogyakarta yang bertujuan mengembangkan dan menyebarluaskan pengetahuan tentang teknologi komputer dan informatika di kalangan masyarakat Indonesia melalui usaha pendidikan yang sistematis dan ilmiah. Yayasan tersebut mengelola sebuah akademi yang bernama Akademi Aplikasi Komputer, yang kemudian disingkat menjadi AKAKOM.
+Landmark STMIK Akakom tahun 1992
+Kemudian pada tanggal 31 Maret 1983, Akademi Aplikasi Komputer (AKAKOM), diubah menjadi Akademi Komputer dan Informatika Akakom. Pada tanggal 2 Mei 1985, nama Akademi Komputer dan Informatika Akakom diubah dan dibakukan menjadi Akademi Manajemen Informatika dan Komputer (AMIK) Akakom. AMIK Akakom kemudian berubah menjadi Sekolah Tinggi Manajemen Informatika dan Komputer (STMIK) Akakom pada tanggal 8 Juni 1992, berdasarkan Surat Keputusan Direktorat Jenderal Pendidikan Tinggi Departemen Pendidikan dan Kebudayaan RI Nomor 262/DIKTI/Kep/1992 dengan status terdaftar bagi program sarjana dan status diakui bagi program diplomanya.
+
+Pada tanggal 27 Oktober 2021, STMIK Akakom, berubah menjadi Universitas Teknologi Digital Indonesia (UTDI), berdasarkan Surat Keputusan (SK) Menteri Pendidikan Kebudayaan Riset dan Teknologi RI Nomor 463/E/O/221 Tentang Izin Perubahan Bentuk dari Sekolah Tinggi Bentuk Menjadi Universitas.</p>
+      </div>
+      <div class="col-md-5">
+        <img src="Landmark_Universitas_Teknologi_Digital_Indonesia_(UTDI).jpg" class="img-rounded" alt="Cinque Terre" width="418" height="400">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+    </div>
+
+  </div><!-- /.container -->
+
+
+  <!-- FOOTER -->
+  <footer class="container">
+    <p class="float-end"><a href="#">Back to top</a></p>
+    <p>&copy; 2017–2022 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </footer>
+</main>
+
+
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      
+  </body>
+</html>
